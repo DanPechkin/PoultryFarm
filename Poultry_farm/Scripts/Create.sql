758fb21e1889174163af80614b1f929b4546d8d8
DanPechkin <77912362+DanPechkin@users.noreply.github.com>
2022-03-01 19:04:38 +0300

    Created new repository

---
+++ Poultry_farm/Scripts/Create.sql
@@ -0,0 +1,118 @@
+﻿/*
+База данных "Птицефабрика"
+
+Ориентирована на администрацию птицефабрики и позволяющую работать с 
+информацией о работниках фабрики и об имеющихся на ней курах, содержащая следующую информацию 
+   * вес курицы, 
+   * возраст, 
+   * порода, 
+   * количество ежемесячно получаемых от курицы яиц, 
+   * информация о местонахождении курицы
+Сведения о породе включают: 
+   * название породы, 
+   * среднее количество яиц в месяц (производительность) 
+   * средний вес, 
+   * номер рекомендованной диеты. 
+Сведения о клекте:
+    * номер цеха
+    * номер ряда в цеху
+    * номер клетки в ряду
+Сведения о работниках:
+    * номер паспорта
+    * зарплата
+    * закрепленнне за работником клетки
+
+
+*/
+-- удаление таблиц 
+drop table if exists Workshop
+drop table if exists Workers
+drop table if exists Chickens
+drop table if exists Cages
+drop table if exists Breeds 
+
+--диета 
+create table Diets 
+(
+    Id     int not null primary key identity (1,1), 
+    Number int not null 
+);
+
+-- порода кур 
+create table Breeds (
+        Id                int          not null primary key identity (1,1),
+        BreedName         nvarchar(60) not null,                            -- название породы 
+        AverageEggsNumber int          not null,                            -- среднее количество яиц
+        AverageWeight     float        not null,                            -- средний вес курицы
+        IdDietNumber      int          not null                             -- внешний ключ, номер диеты
+
+        constraint FK_Breeds_Diet foreign key (IdDietNumber) references dbo.Diets(Id)
+        constraint CK_Breed_Average_Weight check ([AverageWeight] >=0)
+);
+
+--клектка 
+create table Cages (
+        Id                 int   not null  primary key identity (1,1),
+        IdWorkshop         int   not null,                           -- внешний ключ, связь с цехом
+        IdChicken          int   not null,                           -- внешний ключ, связь с курицей
+        IdWorker           int   not null,                           -- внешний ключ, с работником 
+        RowNumber          int   not null,                           -- номер ряда в цеху
+        CageNumber         int   not null,                           -- номер клетки в ряду  
+
+       --внешние ключи 
+       constraint FK_Cages_Workshop foreign key (IdWorkshop) references dbo.Workshop(Id),
+       constraint FK_Cages_Chicken  foreign key (IdChicken)  references dbo.Chichens(Id), 
+       constraint FK_Cages_Worker  foreign key (IdWorker)   references dbo.Worker(Id)
+        
+);
+
+-- курица
+create table Chickens (
+        Id                 int   not null  primary key identity (1,1),
+        IdBreed            int   not null,                            -- внешний ключ, ссылка на порода
+        ChickenWeight      float not null,                            -- вес курицы
+        ChickenAge         int   not null,                            -- возраст курицы в месяцах
+        
+
+ --внешние ключи 
+       constraint FK_Chicken_Breed foreign key (IdBreed) references dbo.Breeds(Id),
+       constraint FK_Chicken_Cage  foreign key (IdCage)  references dbo.Cages(Id)
+);
+
+ 
+
+-- работник птицефабрики
+create table Workers (
+        Id                  int          not null primary key identity (1,1),
+        IdWorkshop          int          not null,                        --внешний ключ для цеха
+        Surname             nvarchar(60) not null,                       -- фамилия
+        [Name]              nvarchar(60) not null,                       -- имя
+        Patronymic          nvarchar(60) not null,                       -- отчество
+        Passport            nvarchar(15) not null,                       -- номер паспорта 
+        Salary              int          not null,                       -- зарплата
+        Fired               bit                                          -- признак уволенности  
+        --внешний ключ
+    constraint FK_Workers_Workshop foreign key (IdWorkshop) references dbo.Workshop(Id)
+
+        
+);
+
+-- цех
+create table Workshop (
+        Id                int          not null primary key identity(1,1),
+        ShopName          nvarchar(60) not null,          -- название цеха
+        NumberOfRows      int          not null,          -- количество рядов
+        NumberOfCages     int          not null           -- количество клеток
+);
+
+--производство 
+create table Production 
+(     
+        Id             int    not null primary key identity(1,1), 
+        IdChicken      int    not null,                      -- внешний ключ 
+        [Data]         date   not null,                      -- дата
+        NumberOfEggs   int    not null                       -- количество яиц
+
+    -- внешний ключ
+    constraint FK_Production_Chicken foreign key (IdChicken) references dbo.Chickens(Id)
+);
---
+++ Poultry_farm/Scripts/Queries.sql
@@ -0,0 +1,79 @@
+﻿
+--Запрос 1.  Количество яиц от каждой курицы данного веса, породы и возраста 
+declare @weight float = 2.7, @breed nvarchar(max) = N'Русская белая', @age int =1
+select * from  Chickens
+      Join Breeds on Chickens.IdBreed = Breeds.Id
+where 
+      Chickens.ChickenWeight = @weight and 
+      Chickens.ChickenAge = @age and
+      Breeds.BreedName = @breed
+go
+
+--Запрос 2. В каком цеху наибольшее количество кур определенной породы? 
+declare @breed nvarchar(max) = N'Русская белая'
+
+select 
+    Top(1)
+    BreedName
+    ,WorkshopNumber
+    ,Count(Chickens.Id) as CountMax
+from 
+    Chickens
+    Join Cages on Chickens.IdCage = Cages.Id
+    Join Breeds on Chickens.IdBreed = Breeds.Id
+Group by WorkshopNumber,
+         BreedName
+Having BreedName = @breed 
+Order by  CountMax desc
+
+--Запрос 3. В каких клетках находятся куры указанного возраста с заданным номером диеты? 
+declare @age int = 15, @diet int  = 2 
+select 
+     Cages.Id
+     ,ChickenAge
+     ,DietNumber
+from 
+    Chickens
+    Join Cages on Chickens.IdCage = Cages.Id
+    Join Breeds on Chickens.IdBreed = Breeds.Id
+Where
+    ChickenAge = @age and DietNumber = @diet
+
+-- Запрос 4. Сколько яиц в день приносят куры, указанного работника
+
+-- Запрос 6. В каком цеху курица, от которой получают больше всего яиц
+select 
+      Top(1)
+     Chickens.Id
+     , Cages.WorkshopNumber
+     , EggsNumber
+from 
+     Chickens
+     Join Cages on Chickens.IdCage = Cages.Id
+     Join Breeds on Chickens.IdBreed = Breeds.Id
+Order by EggsNumber desc
+
+-- Запрос 7 Сколько кур каждой породы в каждом цехе?
+select 
+      BreedName
+     ,Count(Breeds.BreedName) as BreedNumber
+     , Cages.WorkshopNumber
+from 
+    Chickens
+    Join Cages on Chickens.IdCage = Cages.Id
+    Join Breeds on Chickens.IdBreed = Breeds.Id
+Group by
+        BreedName,
+        WorkshopNumber
+
+ -- Запрос 8. Какое количество кур обслуживает каждый работник
+ select 
+       Count (Workshop.IdCage) as ChickenNumber
+       , Workers.Id
+from 
+         
+   
+     
+
+      
+       
